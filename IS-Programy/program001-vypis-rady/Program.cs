76f654c77f0aa4a4d2ea01b24cf3b7cf3ec2cf41
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-15 09:30:44 +0200

    #3 Číselná řada - výpis vstupních hodnot

--- IS-Programy/program001-vypis-rady/Program.cs
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -35,7 +35,23 @@ while (again == "a")
         Console.Write("Nezadali jste celé číslo. Zadejte diferenci znovu: ");
     }
 
+    // Výpis vstupních hodnot
+    Console.WriteLine();
+    Console.WriteLine("====================================");
+    Console.WriteLine("Zadali jste tyto hodnoty: ");
+    Console.WriteLine("První číslo řady: {0}", first);
+    Console.WriteLine("Poslední číslo řady: {0}", last);
+    Console.WriteLine("Diference: {0}", step);
+    Console.WriteLine("====================================");
+    Console.WriteLine("První číslo: {0}; Poslední číslo: {1}; Diference: {2}", first, last, step);
+    Console.WriteLine("====================================");
+    Console.WriteLine();
+
+
     
+    
+
+
 
 
 
