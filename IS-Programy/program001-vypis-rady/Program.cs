475c509294453e865bbfa5b98668556b000126f7
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-15 09:18:40 +0200

    #3 Číselná řada - vstupy

--- IS-Programy/program001-vypis-rady/Program.cs
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -16,11 +16,29 @@ while (again == "a")
     //Vstup hodnoty do programu - řešený správně
     Console.Write("Zadejte první číslo řady (celé číslo): ");
     int first;
-
     while (!int.TryParse(Console.ReadLine(), out first))
     {
         Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
     }
+
+    Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+    int last;
+    while (!int.TryParse(Console.ReadLine(), out last))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
+    }
+
+    Console.Write("Zadejte diferenci (celé číslo): ");
+    int step;
+    while (!int.TryParse(Console.ReadLine(), out step))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte diferenci znovu: ");
+    }
+
+    
+
+
+
 
 
     Console.WriteLine();
