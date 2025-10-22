c75b611636f0f55db8d990f8c3bb21d456cf7ad9
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-22 10:24:19 +0200

    #6 obdélník - vykreslení

--- IS-Programy/program004a-obdelnik/Program.cs
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************");
+    Console.WriteLine("***** Vykreslení obdélníku *****");
+    Console.WriteLine("********************************");
+    Console.WriteLine("********* Tomáš Žižka **********");
+    Console.WriteLine("********************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte šířku obrazce (celé číslo): ");
+    int width;
+
+    while (!int.TryParse(Console.ReadLine(), out width))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte šířku obrazce znovu: ");
+    }
+
+    Console.Write("Zadejte výška obrazce (celé číslo): ");
+    int height;
+
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte výšku obrazce znovu: ");
+    }
+
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= width; j++)
+        {
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(150)); // zpoždění
+        }
+        Console.WriteLine();
+    }
+        
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+
+}
+
+
+
