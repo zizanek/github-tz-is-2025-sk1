3675bdbb936c50c413f69e0a5d19072caa07ddfc
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-22 10:00:41 +0200

    #5 nejvetsi ze tri cisel - program

--- IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
+++ IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("**********************************");
+    Console.WriteLine("***** Největšíí ze tří čísel *****");
+    Console.WriteLine("**********************************");
+    Console.WriteLine("*********** Tomáš Žižka **********");
+    Console.WriteLine("**********************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte celočíselnou hodnotu A: ");
+    int a;
+
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu A znovu: ");
+    }
+
+    Console.Write("Zadejte celočíselnou hodnotu B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu B znovu: ");
+    }
+
+    Console.Write("Zadejte celočíselnou hodnotu C: ");
+    int c;
+    while (!int.TryParse(Console.ReadLine(), out c))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu C znovu: ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("===========================================================");
+    if (a > b)
+    {
+        if (a > c)
+            Console.WriteLine($"Největší číslo je A = {a}");
+        else
+            Console.WriteLine($"Největší číslo je C = {c}");
+    }
+    else
+    {
+        if (b > c)
+            Console.WriteLine($"Největší číslo je B = {b}");
+        else
+            Console.WriteLine($"Největší číslo je C = {c}");
+    }
+    Console.WriteLine("===========================================================");
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+}
+
+
+
