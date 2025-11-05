88a3ba860980c318e13c47cbe0ed6af944e34ee2
Tomáš Žižka <tomas.zizka@tul.cz>
2025-11-05 10:21:32 +0100

    #7 generátor - kladná a záporná

--- IS-Programy/program005-generator/Program.cs
+++ IS-Programy/program005-generator/Program.cs
@@ -45,14 +45,59 @@ while (again == "a")
     //Random myRandNumb = new Random(50); // generování stejných čísel při stejném vstupu - hodí se pro testování
     Random myRandNumb = new Random();
 
+    // záporná, kladná, nuly
+    int negativeNumbs = 0; //záporná čísla
+    int positiveNumbs = 0; //kladná čísla
+    int zeros = 0; //nuly
+
+    // sudá a lichá
+    int evenNumbs = 0;
+    int oddNumbs = 0;
+
+
+
+
     Console.WriteLine();
     Console.WriteLine("=============================================");
     Console.WriteLine("Pseudonáhodná čísla:");
-    for (int i = 0; i < n ; i++)
+    for (int i = 0; i < n; i++)
     {
-        myRandNumbs[i] = myRandNumb.Next(lowerBound, upperBound);
+        myRandNumbs[i] = myRandNumb.Next(lowerBound, upperBound + 1);
         Console.Write("{0}; ", myRandNumbs[i]);
+
+        /*if (myRandNumbs[i] < 0)
+            negativeNumbs++;
+        if (myRandNumbs[i] > 0)
+            positiveNumbs++;
+        if (myRandNumbs[i] == 0)
+            zeros++;            */
+
+        if (myRandNumbs[i] < 0)
+            negativeNumbs++;
+        else if (myRandNumbs[i] > 0)
+            positiveNumbs++;
+        else
+            zeros++;
+
+        if (myRandNumbs[i] % 2 == 0)
+            evenNumbs++;
+        else
+            oddNumbs++;    
+
     }
+
+    Console.WriteLine();
+    Console.WriteLine("=============================================");
+    Console.WriteLine("=============================================");
+    Console.WriteLine("Počet záporných: {0}", negativeNumbs);
+    Console.WriteLine("Počet kladných: {0}", positiveNumbs);
+    Console.WriteLine("Počet nul: {0}", zeros);
+    Console.WriteLine("=============================================");
+    Console.WriteLine("Počet sudých: {0}", evenNumbs);
+    Console.WriteLine("Počet lichých: {0}", oddNumbs);
+    Console.WriteLine("=============================================");
+    Console.WriteLine("=============================================");
+
 
     
     Console.WriteLine();
