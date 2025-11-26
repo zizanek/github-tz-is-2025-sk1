294e64125efc52b6b6928c990a3715e71ab6df75
Tomáš Žižka <tomas.zizka@tul.cz>
2025-11-26 10:35:08 +0100

    #11 Převod 10to2 - převod - s chybou přetečení pole

--- IS-Programy/program009a-10to2/Program.cs
+++ IS-Programy/program009a-10to2/Program.cs
@@ -3,23 +3,39 @@ while (again == "a")
 {
     Console.Clear();
     Console.WriteLine("****************************");
-    Console.WriteLine("***** Název programu *****");
+    Console.WriteLine("***** Převod z 10 do 2 soustavy *****");
     Console.WriteLine("****************************");
     Console.WriteLine("******* Tomáš Žižka ********");
     Console.WriteLine("****************************");
     Console.WriteLine();
 
-    // Vstup hodnoty do programu - špatně řešený
-    //Console.Write("Zadejte první číslo řady: ");
-    //int first = int.Parse(Console.ReadLine());
-
     //Vstup hodnoty do programu - řešený správně
-    Console.Write("Zadejte hodnotu (celé číslo): ");
-    int first;
+    Console.Write("Zadejte hodnotu (přirozené číslo) - desítkové číslo: ");
+    uint number10;
 
-    while (!int.TryParse(Console.ReadLine(), out first))
+    while (!uint.TryParse(Console.ReadLine(), out number10))
     {
-        Console.Write("Nezadali jste celé číslo. Zadejte hodnotu znovu: ");
+        Console.Write("Nezadali jste přirozené číslo. Zadejte hodnotu znovu (přirozené číslo) - desítkové číslo: ");
+    }
+
+    uint backupNumber10 = number10;
+    uint zbytek;
+
+    uint[] myArray = new uint[32];
+
+    uint i;
+    for (i = 0; number10 > 0; i++)
+    {
+        zbytek = number10 % 2;
+        number10 = (number10 - zbytek) / 2;
+        myArray[i] = zbytek;
+
+        Console.WriteLine("Celá část = {0}; Zbytek = {1}", number10, zbytek);
+    }
+
+    Console.WriteLine();
+    for (uint j=i-1;j>=0;j--) {
+        Console.Write("{0}", myArray[j]);
     }
 
 
