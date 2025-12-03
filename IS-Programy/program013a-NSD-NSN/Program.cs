19a5cdd103f636fded1a63c27f71065ce72bd59f
Tomáš Žižka <tomas.zizka@tul.cz>
2025-12-03 10:32:57 +0100

    #15 NSD a NSN - metoda pro načítání hodnot

--- IS-Programy/program013a-NSD-NSN/Program.cs
+++ IS-Programy/program013a-NSD-NSN/Program.cs
@@ -8,7 +8,11 @@ while (again == "a")
 
     // Metoda pro razítko - hlavičku
     razitko();
-    
+
+    // Načítání hodnot
+    ulong a = nactiCislo("Zadejte číslo a: ");
+    ulong b = nactiCislo("Zadejte číslo b: ");
+
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
     again = Console.ReadLine();
@@ -27,6 +31,20 @@ static void razitko()
     Console.WriteLine();
 }    
 
+/* metoda pro načtení čísel */
+static ulong nactiCislo(string zprava)
+{
+    Console.Write(zprava);
+    ulong cislo;
+    while (!ulong.TryParse(Console.ReadLine(), out cislo))
+    {
+        Console.Write("Nezadali jste přirozené číslo. Zadejte hodnotu znovu: ");
+    }
+
+    // !!! Metoda, která vrací nějaký konkrétní datový typ, musí obsahovat následující řádek
+    return cislo;
+
+}
 
 
 
