75b9f6034da5bea258e51206864b4f6b3189c10c
Tomáš Žižka <tomas.zizka@tul.cz>
2025-12-03 10:19:50 +0100

    #15 NSD a NSN - metoda razitko

--- IS-Programy/program013a-NSD-NSN/Program.cs
+++ IS-Programy/program013a-NSD-NSN/Program.cs
@@ -1,16 +1,33 @@
 ﻿
+/* Hlavní program - začátek */
+
 string again = "a";
 while (again == "a")
 {
     Console.Clear();
 
-
+    // Metoda pro razítko - hlavičku
+    razitko();
+    
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
     again = Console.ReadLine();
-
-
 }
+
+/* Hlavní program - konec */
+
+/* metoda která nic nevrací - nevrací hodnotu */
+static void razitko()
+{
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Výpočet NSD a NSN *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Tomáš Žižka ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+}    
+
+
 
 
 
