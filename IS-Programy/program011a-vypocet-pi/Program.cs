1509befccafcc21fa343b06076626d5d924d8c51
Tomáš Žižka <tomas.zizka@tul.cz>
2025-12-03 09:22:02 +0100

    #13 Vypocet PI - vypocet a vizualizace vysledku

--- IS-Programy/program011a-vypocet-pi/Program.cs
+++ IS-Programy/program011a-vypocet-pi/Program.cs
@@ -19,6 +19,22 @@ while (again == "a")
     double znamenko = 1;
     double piCtvrt = 1;
 
+    while((1/i)>=presnost) {
+        i = i + 2;
+        znamenko = -znamenko;
+        piCtvrt = piCtvrt + znamenko * (1/i);
+
+        if(znamenko==1) {
+            Console.WriteLine("Zlomek: +1/{0}; aktuální hodnota PI = {1}", i, 4 * piCtvrt);
+        }
+        else {
+            Console.WriteLine("Zlomek: -1/{0}; aktuální hodnota PI = {1}", i, 4 * piCtvrt);
+        }
+    }
+
+    Console.WriteLine("\n\n Hodnota čísla PI = {0}", 4 * piCtvrt);
+    //Console.WriteLine("\n\n Hodnota čísla PI = {0:f4}", 4 * piCtvrt);
+
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
     again = Console.ReadLine();
