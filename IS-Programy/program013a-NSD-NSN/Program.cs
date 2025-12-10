92aa2ca903de63de72feccc90481506fce2abbcd
Tomáš Žižka <tomas.zizka@tul.cz>
2025-12-10 09:45:05 +0100

    #15 NSD a NSN pomocí vlastních metod

--- IS-Programy/program013a-NSD-NSN/Program.cs
+++ IS-Programy/program013a-NSD-NSN/Program.cs
@@ -8,12 +8,16 @@ while (again == "a")
 
     // Metoda pro razítko - hlavičku
     razitko();
-
+    
     // Načítání hodnot
     ulong a = nactiCislo("Zadejte číslo a: ");
     ulong b = nactiCislo("Zadejte číslo b: ");
 
-    Console.WriteLine();
+    ulong nsd = vypocitatNSD(a, b);
+    ulong nsn = vypocitatNSN(a, b, nsd); 
+
+    zobrazitVysledky(a, b, nsd, nsn);
+
     Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
     again = Console.ReadLine();
 }
@@ -46,8 +50,34 @@ static ulong nactiCislo(string zprava)
 
 }
 
+/* metoda pro výpočet NSD dvou čísel */
+static ulong vypocitatNSD(ulong a, ulong b) {
+    while(a != b ) {
+        if(a > b)
+            a = a - b;
+        else
+            b = b - a;    
+    }
+    return a;   
+}
 
+/* metoda pro výpočet NSN na základě známé hodnoty NSD */
+static ulong vypocitatNSN(ulong a, ulong b, ulong nsd) {
+    return (a*b)/nsd;
+}
 
+/* metoda pro zobrazení výsledků */
+static void zobrazitVysledky(ulong a, ulong b, ulong nsd, ulong nsn) {
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine("================================");
+    Console.WriteLine("NSD čísel {0} a {1} je {2}", a, b, nsd);
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("================================");
+    Console.WriteLine("NSN čísel {0} a {1} je {2}", a, b, nsn);
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.White;
+}
 
 
 
